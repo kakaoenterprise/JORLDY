6de471371ad11780c4ceefecf6981d690ee0e78a
kakao_ent <kakao_ent@kakao-entui-MacBookPro.local>
2020-10-09 16:37:52 +0900

    EDIT buffer shape change

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -12,13 +12,18 @@ class ReplayBuffer:
     def sample(self, batch_size):
         batch = random.sample(self.buffer, batch_size)
         
-        state       = torch.tensor([b[0] for b in batch]).float().view(batch_size, -1)
-        action      = torch.tensor([b[1] for b in batch]).float().view(batch_size, -1)
-        reward      = torch.tensor([b[2] for b in batch]).float().view(batch_size, -1)
-        next_state  = torch.tensor([b[3] for b in batch]).float().view(batch_size, -1)
-        done        = torch.tensor([b[4] for b in batch]).float().view(batch_size, -1)
+        state       = torch.tensor([b[0] for b in batch]).float()
+        action      = torch.tensor([b[1] for b in batch]).float()
+        reward      = torch.tensor([b[2] for b in batch]).float()
+        next_state  = torch.tensor([b[3] for b in batch]).float()
+        done        = torch.tensor([b[4] for b in batch]).float()
 
-        return (state, action, reward, next_state, done)
+        transitions = []
+        for item in [state, action, reward, next_state, done]:
+            if len(list(item.shape)) == 1:
+                item = torch.unsqueeze(item, 1) 
+            transitions.append(item)
+        return transitions
     
     @property
     def length(self):
