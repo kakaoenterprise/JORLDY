b88b34c6cd1cd64135ddee6674ba8d317aa9c827
root <root@gpu-cloud-vnode124.dakao.io>
2021-01-14 14:56:07 +0900

    EDIT cnn dimension

--- core/network/dqn.py
+++ core/network/dqn.py
@@ -21,11 +21,14 @@ class DQN_CNN(torch.nn.Module):
         super(DQN_CNN, self).__init__()
         self.D_in = D_in
         self.D_out = D_out
-                
-        self.conv1 = torch.nn.Conv2d(in_channels=self.D_in[0], out_channels=32, kernel_size=8, stride=4, padding=4)
-        self.conv2 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=2)
-        self.conv3 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=1)
-        self.fc1 = torch.nn.Linear(64*(self.D_in[1]//8)*(self.D_in[2]//8), 512)
+        self.conv1 = torch.nn.Conv2d(in_channels=self.D_in[0], out_channels=32, kernel_size=8, stride=4)
+        dim1 = ((self.D_in[1] - 8)//4 + 1, (self.D_in[2] - 8)//4 + 1)
+        self.conv2 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
+        dim2 = ((dim1[0] - 4)//2 + 1, (dim1[1] - 4)//2 + 1)
+        self.conv3 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+        dim3 = ((dim2[0] - 3)//1 + 1, (dim2[1] - 3)//1 + 1)
+        
+        self.fc1 = torch.nn.Linear(64*dim3[0]*dim3[1], 512)
         self.fc2 = torch.nn.Linear(512, self.D_out)
         
     def forward(self, x):
@@ -79,12 +82,15 @@ class Dueling_CNN(torch.nn.Module):
         self.D_in = D_in
         self.D_out = D_out
         
-        self.conv1 = torch.nn.Conv2d(in_channels=self.D_in[0], out_channels=32, kernel_size=8, stride=4, padding=4)
-        self.conv2 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=2)
-        self.conv3 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=1)
+        self.conv1 = torch.nn.Conv2d(in_channels=self.D_in[0], out_channels=32, kernel_size=8, stride=4)
+        dim1 = ((self.D_in[1] - 8)//4 + 1, (self.D_in[2] - 8)//4 + 1)
+        self.conv2 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
+        dim2 = ((dim1[0] - 4)//2 + 1, (dim1[1] - 4)//2 + 1)
+        self.conv3 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
+        dim3 = ((dim2[0] - 3)//1 + 1, (dim2[1] - 3)//1 + 1)
         
-        self.fc1_a = torch.nn.Linear(64*(self.D_in[1]//8)*(self.D_in[2]//8), 512)
-        self.fc1_v = torch.nn.Linear(64*(self.D_in[1]//8)*(self.D_in[2]//8), 512)
+        self.fc1_a = torch.nn.Linear(64*dim3[0]*dim3[1], 512)
+        self.fc1_v = torch.nn.Linear(64*dim3[0]*dim3[1], 512)
 
         self.fc2_a = torch.nn.Linear(512, self.D_out)
         self.fc2_v = torch.nn.Linear(512, 1)
@@ -111,39 +117,3 @@ class Dueling_CNN(torch.nn.Module):
         out = x_a + x_v # [bs, num_action]
         return out
     
-class C51(torch.nn.Module):
-    def __init__(self, D_in, D_out, D_hidden=512):
-        super(C51, self).__init__()
-        self.D_in = D_in
-        self.D_out = D_out
-        
-        self.l1 = torch.nn.Linear(D_in, D_hidden)
-        self.l2 = torch.nn.Linear(D_hidden, D_hidden)
-        self.q = torch.nn.Linear(D_hidden, D_out)
-
-    def forward(self, x):
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-        return self.q(x)
-    
-class C51_CNN(torch.nn.Module):
-    def __init__(self, D_in, D_out):
-        super(C51_CNN, self).__init__()
-        self.D_in = D_in
-        self.D_out = D_out
-        
-        self.conv1 = torch.nn.Conv2d(in_channels=self.D_in[0], out_channels=32, kernel_size=8, stride=4, padding=4)
-        self.conv2 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2, padding=2)
-        self.conv3 = torch.nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=1, padding=1)
-        self.fc1 = torch.nn.Linear(64*(self.D_in[1]//8)*(self.D_in[2]//8), 512)
-        self.fc2 = torch.nn.Linear(512, self.D_out*self.Num_support)
-        
-    def forward(self, x):
-        x = (x-(255.0/2))/(255.0/2)
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
