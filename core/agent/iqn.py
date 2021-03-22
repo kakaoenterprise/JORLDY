809e1ea994d0e7a985ed6d7e55c7c226bb022fa8
ramanuzan.lee <root@gpu-cloud-vnode206.dakao.io>
2021-03-22 16:25:49 +0900

    UPDATE IQN and matmul -> @

--- core/agent/iqn.py
+++ core/agent/iqn.py
@@ -56,26 +56,75 @@ class IQNAgent(QRDQNAgent):
         self.sample_min = sample_min
         self.sample_max = sample_max
         
-        # Get tau
-        min_tau = 1/(2*self.num_support)
-        max_tau = (2*self.num_support+1)/(2*self.num_support)
-        self.tau = torch.arange(min_tau, max_tau, 1/self.num_support, device=device).view(1, self.num_support)
-        self.inv_tau = 1 - self.tau
-
-
     def act(self, state, training=True):
         self.network.train(training)
         epsilon = self.epsilon if training else self.epsilon_eval
         sample_min = 0 if training else self.sample_min
-        sample_max = 0 if training else self.sample_max
+        sample_max = 1 if training else self.sample_max
         
         if np.random.random() < epsilon:
             action = np.random.randint(0, self.action_size, size=(state.shape[0], 1))
         else:
-            logits = self.network(torch.FloatTensor(state).to(device), sample_min, sample_max)
+            logits, _ = self.network(torch.FloatTensor(state).to(device), sample_min, sample_max)
             _, q_action = self.logits2Q(logits)
             action = torch.argmax(q_action, -1, keepdim=True).data.cpu().numpy()
         return action
+
+    def learn(self):
+        if self.memory.size < max(self.batch_size, self.start_train_step):
+            return None
+        
+        transitions = self.memory.sample(self.batch_size)
+        state, action, reward, next_state, done = map(lambda x: torch.FloatTensor(x).to(device), transitions)
+        
+        # Get Theta Pred, Tau
+        logit, tau = self.network(state)
+        logits, q_action = self.logits2Q(logit)
+        action_eye = torch.eye(self.action_size, device=device)
+        action_onehot = action_eye[action.long()]
+
+        theta_pred = action_onehot @ logits
+        tau = torch.transpose(tau, 1, 2)
+        
+        with torch.no_grad():
+            # Get Theta Target 
+            logit_next, _ = self.network(next_state)
+            _, q_next = self.logits2Q(logit_next)
+
+            logit_target, _ = self.target_network(next_state)
+            logits_target, _ = self.logits2Q(logit_target)
+            
+            max_a = torch.argmax(q_next, axis=-1, keepdim=True)
+            max_a_onehot = action_eye[max_a.long()]
+
+            theta_target = reward + (1-done) * self.gamma * torch.squeeze(max_a_onehot @ logits_target, 1)
+            theta_target = torch.unsqueeze(theta_target, 2)
+        
+        error_loss = theta_target - theta_pred 
+        huber_loss = F.smooth_l1_loss(theta_target, theta_pred, reduction='none')
+        
+        # Get Loss
+        loss = torch.where(error_loss < 0.0, 1-tau, tau) * huber_loss
+        loss = torch.mean(torch.sum(loss, axis = 2))
+        
+        max_Q = torch.max(q_action).item()
+        max_logit = torch.max(logit).item()
+        min_logit = torch.min(logit).item()
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        
+        self.num_learn += 1
+
+        result = {
+            "loss" : loss.item(),
+            "epsilon" : self.epsilon,
+            "max_Q": max_Q,
+            "max_logit": max_logit,
+            "min_logit": min_logit,
+        }
+        return result
     
     def logits2Q(self, logits):
         _logits = torch.transpose(logits, 1, 2)
