2eb7c95937580292537c6af1679de641ae9a78b8
root <root@gpu-cloud-vnode81.dakao.io>
2021-07-21 19:06:23 +0900

    UPDATE atari add reward_scale and edit dead_penalty

--- core/env/nes.py
+++ core/env/nes.py
@@ -2,14 +2,14 @@ import numpy as np
 # https://pypi.org/project/gym-super-mario-bros/
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+from gym_super_mario_bros.actions import RIGHT_ONLY
 
 from .atari import Atari
 
 class Nes(Atari):
     def __init__(self, name, **kwargs):
         super(Nes, self).__init__(name=name, life_key='life', **kwargs)
-        self.env = JoypadSpace(self.env, SIMPLE_MOVEMENT)
+        self.env = JoypadSpace(self.env, RIGHT_ONLY)
         print(f"action size changed: {self.action_size} -> {self.env.action_space.n}")
         self.action_size = self.env.action_space.n
         
@@ -18,7 +18,8 @@ class Nes(Atari):
 
 class Mario(Nes):
     def __init__(self, **kwargs):
-        super(Mario, self).__init__('SuperMarioBros-v2', **kwargs)
+        reward_scale = 15.
+        super(Mario, self).__init__('SuperMarioBros-v2', reward_scale=reward_scale, **kwargs)
 
 if __name__=="__main__":
     env = Mario()
