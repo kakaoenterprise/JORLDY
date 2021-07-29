2ff9e67ddeea49ce500e2011fe04779b85b23f54
root <root@gpu-cloud-vnode146.dakao.io>
2021-07-29 15:51:19 +0900

    RND_PPO: main_debug_rnd.py now can save episode as gif with intrinsic reward printed on it

---
+++ config/rnd_ppo/mario-debug.py
@@ -0,0 +1,52 @@
+### RND PPO Mario Config ###
+
+env = {
+    "name": "mario",
+    "render": False,
+    "gray_img": True,
+    "img_width": 84,
+    "img_height": 84,
+    "stack_frame": 4,
+#     "no_op": True,
+    "reward_clip": True,
+    "dead_penalty": False,
+}
+
+agent = {
+    "name":"rnd_ppo",
+    "network":"discrete_pi_v_cnn",
+    "optimizer":"adam",
+    "learning_rate": 2.5e-4,
+    "gamma":0.99,
+    "batch_size":32,
+    "n_step": 128,
+    "n_epoch": 3,
+    "_lambda": 0.95,
+    "epsilon_clip": 0.1,
+    "vf_coef": 1.0,
+    "ent_coef": 0.01,
+    "clip_grad_norm": 1.0,
+    # Parameters for Random Network Distillation
+    "rnd_network": "rnd_cnn",
+    "gamma_i": 0.99,
+    "extrinsic_coeff": 1.0,
+    "intrinsic_coeff": 10.0,
+    "obs_normalize": True,
+    "ri_normalize": True,
+#     "ri_normalize": False,
+}
+
+train = {
+    "training" : True,
+    "load_path" : "./logs/mario/rnd_ppo/20210726152042/",
+    "run_step" : 30000000,
+    "print_period" : 10000,
+    "save_period" : 100000,
+#     "save_period" : 10000,
+    "test_iteration": 1,
+    "record" : True,
+    "record_period" : 200000,
+    # distributed setting
+    "update_period" : agent["n_step"],
+    "num_worker" : 32,
+}
--- main_debug_rnd.py
+++ main_debug_rnd.py
@@ -3,11 +3,16 @@ import argparse
 from core import *
 from manager import *
 
+from collections import deque
 import matplotlib.pyplot as plt 
 import numpy as np
+import imageio
+import cv2
+import torch
 
 # default_config_path = "config.YOUR_AGENT.YOUR_ENV"
-default_config_path = "config.rnd_dqn.mario"
+# default_config_path = "config.icm_ppo.mario"
+default_config_path = "config.rnd_ppo.mario-debug"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -31,32 +36,40 @@ if __name__ == "__main__":
     count_step = 0
     x_plot = []
     y_plot = []
+    frames = []
+    r_is = deque(maxlen=20)
     score = 0
     
     for step in range(1, config.train.run_step+1):
         action = agent.act(state, config.train.training)            
-
         if step < 20:
-            action = np.random.randint(0, 7, size=(1, 1))
-        
+            action = np.random.randint(0, 5, size=(1, 1))
+
         next_state, reward, done = env.step(action)
-        
-        score += reward[0][0]
-        
-        r_i = agent.get_ri(next_state)
+        frames.append(env.get_frame())
+        r_i = agent.rnd.forward(torch.as_tensor(next_state, dtype=torch.float32, device=agent.device))
+        r_is.append(int(r_i*1000))
+        cv2.putText(frames[-1], f"{r_i.item()*1000:.1f}", (10,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+        for i in range(min(step, 20)):
+            cv2.putText(frames[-1], f".", (50+i*2,120-r_is[i]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
         
         x_plot.append(step)
         y_plot.append(r_i.item())
-        
         if done:
-            plt.plot(x_plot, y_plot)
-            plt.savefig('./r_i (' + str(score) +').png')
-            raise Exception('Plot is done')
+            plt.rcParams["figure.figsize"] = (18,6)
+            plt.scatter(x_plot, y_plot, s=0.5)
+            plt.savefig('./r_i (' + str(env.score) +').png')
+
+            imageio.mimwrite(f"./{step:010d}_{env.score}.gif", frames, fps=30)
+            print('Plot is done')
+            break
         
         state = next_state
-
-        if done:
-            episode += 1
-            state = env.reset()
         
     env.close()
+'''
+    def get_ri(self, state, next_state, action):
+        state, next_state, action = map(lambda x: torch.as_tensor(x, dtype=torch.float32, device=self.device), [state, next_state, action])
+        r_i, _, _ = self.icm(state, action, next_state)
+        return r_i
+'''
