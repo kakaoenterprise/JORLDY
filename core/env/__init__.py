7a196e358e8fafb727dcdbaaf59b2bec37bd37f8
root <root@gpu-cloud-anode3.dakao.io>
2021-08-10 16:29:56 +0900

    UPDATE working directory path for multinode training

--- core/env/__init__.py
+++ core/env/__init__.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__))) # for import mlagent
 sys.path.append(os.path.abspath('../../'))
 
 
-working_path = __name__.replace(".","/")
+working_path = os.path.dirname(os.path.realpath(__file__))
 file_list = os.listdir(working_path)
 module_list = [file.replace(".py", "") for file in file_list 
                if file.endswith(".py") and file.replace(".py","") not in ["__init__", "base", "utils"]]
