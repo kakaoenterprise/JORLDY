78116ca66505d6e3610d205c1973e347b4435c1e
root <root@gpu-cloud-cpu23.dakao.io>
2021-08-12 14:38:38 +0900

    UPDATE init

--- manager/__init__.py
+++ manager/__init__.py
@@ -1,4 +1,4 @@
-import os, sys, inspect
+import os, inspect
 
 working_path = os.path.dirname(os.path.realpath(__file__))
 file_list = os.listdir(working_path)
@@ -7,8 +7,8 @@ module_list = [file.replace(".py", "") for file in file_list
 
 for module_name in module_list:
     module_path = f"{__name__}.{module_name}"
-    __import__(module_path, fromlist=[None])
-    for class_name, _class in inspect.getmembers(sys.modules[module_path], inspect.isclass):
+    module = __import__(module_path, fromlist=[None])
+    for class_name, _class in inspect.getmembers(module, inspect.isclass):
         if module_path in str(_class) and "Manager" in class_name:
             exec(f"from {module_path} import {class_name}")
             
