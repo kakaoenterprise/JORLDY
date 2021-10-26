1c1532ac4b52fd3984488fd0d4efcbd3b319b4d5
kakao_ent <kakao_ent@kakao-entui-MacBookPro.local>
2021-10-26 13:58:37 +0900

    UPDATE support partial availability

--- jorldy/core/env/__init__.py
+++ jorldy/core/env/__init__.py
@@ -1,4 +1,4 @@
-import os, sys, inspect, re
+import os, sys, inspect, re, traceback
 from collections import OrderedDict
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__))) # for import mlagents
@@ -11,20 +11,32 @@ module_list = [file.replace(".py", "") for file in file_list
                if file.endswith(".py") and file.replace(".py","") not in ["__init__", "base", "utils"]]
 env_dict = {}
 naming_rule = lambda x: re.sub('([a-z])([A-Z])', r'\1_\2', x).lower()
+all_modules_available = True
 for module_name in module_list:
     module_path = f"{__name__}.{module_name}"
-    module = __import__(module_path, fromlist=[None])
-    for class_name, _class in inspect.getmembers(module, inspect.isclass):
-        if module_path in str(_class) and '_' != class_name[0]:
-            env_dict[naming_rule(class_name)] = _class
+    try:
+        module = __import__(module_path, fromlist=[None])
+    except Exception:
+        print("==========================================================")
+        print(f"============ {module_path} is not available. ==============")
+        print("==========================================================")
+        traceback.print_exc()
+        print("==========================================================")
+        all_modules_available = False
+        continue
+    else:
+        for class_name, _class in inspect.getmembers(module, inspect.isclass):
+            if module_path in str(_class) and '_' != class_name[0]:
+                env_dict[naming_rule(class_name)] = _class
 
-env_dict = OrderedDict(sorted(env_dict.items()))
-with open(os.path.join(working_path, "_env_dict.txt"), 'w') as f:
-    f.write('### Env Dictionary ###\n')
-    f.write('format: (key, class)\n')
-    f.write('------------------------\n')
-    for item in env_dict.items():
-        f.write(str(item) + '\n')
+if all_modules_available:
+    env_dict = OrderedDict(sorted(env_dict.items()))
+    with open(os.path.join(working_path, "_env_dict.txt"), 'w') as f:
+        f.write('### Env Dictionary ###\n')
+        f.write('format: (key, class)\n')
+        f.write('------------------------\n')
+        for item in env_dict.items():
+            f.write(str(item) + '\n')
         
 class Env:
     def __new__(self, name, *args, **kwargs):
