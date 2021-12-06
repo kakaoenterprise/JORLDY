62880fed3ad515869557462d126ef7467bde13bf
Hyunho Lee <ramanuzann@gmail.com>
2021-12-06 11:27:38 +0900

    Update distributed_manager.py

--- jorldy/manager/distributed_manager.py
+++ jorldy/manager/distributed_manager.py
@@ -6,10 +6,10 @@ import ray
 
 class DistributedManager:
     def __init__(self, Env, env_config, Agent, agent_config, num_workers, mode):
-        try:
-            ray.init(address="auto")
-        except:
-            ray.init()
+#         try:
+#             ray.init(address="auto")
+#         except:
+        ray.init()
         agent = Agent(**agent_config)
         self.num_workers = num_workers if num_workers else os.cpu_count()
         Env, env_config, agent = map(ray.put, [Env, dict(env_config), agent])
