26d95df889eb15cf304e86a7cda7b2a4ed0fb348
root <root@gpu-cloud-anode11.dakao.io>
2021-12-13 10:58:42 +0900

    IQN implementation is done

---
+++ jorldy/config/m_iqn/atari.py
@@ -0,0 +1,55 @@
+### Munchausen IQN Atari Config ###
+
+env = {
+    # "name": it should be defined in the command. ex) python main.py --config config.AGENT.atari --env.name breakout
+    "render": False,
+    "gray_img": True,
+    "img_width": 84,
+    "img_height": 84,
+    "stack_frame": 4,
+    "no_op": False,
+    "reward_clip": True,
+    "dead_penalty": True,
+}
+
+agent = {
+    "name": "m_iqn",
+    "network": "iqn",
+    "head": "cnn",
+    "gamma": 0.99,
+    "epsilon_init": 1.0,
+    "epsilon_min": 0.1,
+    "explore_ratio": 0.1,
+    "buffer_size": 1000000,
+    "batch_size": 32,
+    "start_train_step": 100000,
+    "target_update_period": 10000,
+    "num_sample": 64,
+    "embedding_dim": 64,
+    "sample_min": 0.0,
+    "sample_max": 1.0,
+    # M-DQN Parameters
+    "alpha": 0.9,
+    "tau": 0.03, 
+    "l_0": -1,
+}
+
+optim = {
+    "name": "adam",
+    "eps": 1e-2 / agent["batch_size"],
+    "lr": 5e-5,
+}
+
+train = {
+    "training": True,
+    "load_path": None,
+    "run_step": 10000000,
+    "print_period": 10000,
+    "save_period": 100000,
+    "eval_iteration": 5,
+    "record": True,
+    "record_period": 300000,
+    # distributed setting
+    "update_period": 32,
+    "num_workers": 16,
+}
---
+++ jorldy/config/m_iqn/mountaincar.py
@@ -0,0 +1,45 @@
+### Munchausen IQN MountainCar Config ###
+
+env = {
+    "name": "mountain_car",
+    "render": False,
+}
+
+agent = {
+    "name": "m_iqn",
+    "network": "iqn",
+    "gamma": 0.99,
+    "epsilon_init": 1.0,
+    "epsilon_min": 0.1,
+    "explore_ratio": 0.1,
+    "buffer_size": 10000,
+    "batch_size": 64,
+    "start_train_step": 10000,
+    "target_update_period": 500,
+    "num_sample": 64,
+    "embedding_dim": 64,
+    "sample_min": 0.0,
+    "sample_max": 1.0,
+    # M-DQN Parameters
+    "alpha": 0.9,
+    "tau": 0.03, 
+    "l_0": -1,
+}
+
+optim = {
+    "name": "adam",
+    "eps": 1e-2 / agent["batch_size"],
+    "lr": 5e-5,
+}
+
+train = {
+    "training": True,
+    "load_path": None,
+    "run_step": 100000,
+    "print_period": 1000,
+    "save_period": 10000,
+    "eval_iteration": 5,
+    # distributed setting
+    "update_period": 32,
+    "num_workers": 8,
+}
