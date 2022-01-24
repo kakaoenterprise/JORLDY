0af2c16c26fd3bc1f2fadb4b4849ac1867844b28
root <root@gpu-cloud-vnode186.dakao.io>
2022-01-24 10:51:53 +0900

    update icm; update adv part

--- jorldy/core/agent/icm_ppo.py
+++ jorldy/core/agent/icm_ppo.py
@@ -114,14 +114,15 @@ class ICM_PPO(PPO):
                 adv[:, t] += (
                     (1 - done[:, t]) * self.gamma * self._lambda * adv[:, t + 1]
                 )
+
+            ret = adv.view(-1, 1) + value
+
             if self.use_standardization:
                 adv = (adv - adv.mean(dim=1, keepdim=True)) / (
                     adv.std(dim=1, keepdim=True) + 1e-7
                 )
             adv = adv.view(-1, 1)
-            ret = adv + value
 
-        mean_adv = adv.mean().item()
         mean_ret = ret.mean().item()
 
         # start train iteration
@@ -200,7 +201,6 @@ class ICM_PPO(PPO):
             "entropy_loss": np.mean(entropy_losses),
             "max_ratio": max(ratios),
             "min_prob": min(probs),
-            "mean_adv": mean_adv,
             "mean_ret": mean_ret,
             "r_i": r_i.mean().item(),
             "l_f": l_f.item(),
