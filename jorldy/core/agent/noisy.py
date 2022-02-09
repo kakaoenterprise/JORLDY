3ebdd7541656222730993852046e144dae3abd82
ramanuzan <91711496+ramanuzan@users.noreply.github.com>
2022-02-09 16:16:19 +0900

    add noisy lr scheduler (#107)
    Co-authored-by: root <root@gpu-cloud-cpu4.dakao.io>

--- jorldy/core/agent/noisy.py
+++ jorldy/core/agent/noisy.py
@@ -132,6 +132,7 @@ class Noisy(DQN):
 
         if self.memory.size >= self.batch_size and self.time_t >= self.start_train_step:
             result = self.learn()
+            self.learning_rate_decay(step)
 
         # Process per step if train start
         if self.num_learn > 0:
