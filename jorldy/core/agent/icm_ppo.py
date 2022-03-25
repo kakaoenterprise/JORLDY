d67d1ed0fea539565a3db9ca3d9f34a29e1176a5
root <root@gpu-cloud-vnode2.dakao.io>
2022-03-25 14:33:00 +0900

    comment lr decay

--- jorldy/core/agent/icm_ppo.py
+++ jorldy/core/agent/icm_ppo.py
@@ -219,7 +219,7 @@ class ICM_PPO(PPO):
         # Process per epi
         if self.learn_stamp >= self.n_step:
             result = self.learn()
-            self.learning_rate_decay(step)
+            # self.learning_rate_decay(step)
             self.learn_stamp -= self.n_step
 
         return result
