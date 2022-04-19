1301d93ede60a6a9c7f93e16f39cca898777e750
kan-s0 <kan.s@kakaoenterprise.com>
2022-04-19 14:44:11 +0900

    restore cartpole config

--- jorldy/config/muzero/cartpole.py
+++ jorldy/config/muzero/cartpole.py
@@ -26,13 +26,13 @@ agent = {
     "lr_decay": False,
     # out of range state setting
     "enable_after_random_action": True,
-    "enable_prev_random_action": True,
+    "enable_prev_random_action": False,
     "enable_uniform_policy": True,
     # MCTS
     "num_mcts": 30,
     "num_eval_mcts": 15,
     # Self Supervised Consistency Loss
-    "use_ssc_loss": False, 
+    "use_ssc_loss": True, 
 }
 
 optim = {
@@ -51,5 +51,5 @@ train = {
     # distributed setting
     "distributed_batch_size": 128,
     "update_period": 100,
-    "num_workers": 8,
+    "num_workers": 32,
 }
